51f75eb9936772481bad3a5bd5ad57ff5140f5cf
ryanabdallahconrad <ryanconrad1@zohomail.com>
2025-01-10 15:45:27 -0600

    added variable that can be changed to any fruit or "all". used to modify the apiResponse and the link when a server is successfully created

--- main.js
+++ main.js
@@ -1,30 +1,25 @@
 const http = require("node:http");
 const axios = require("axios");
 
-
 const hostname = "127.0.0.1";
 const port = 3000;
 
-
+const fruit = "orange";
 
 // Basically creating the server in a callback function.
 const server = http.createServer(async (request, response) => {
     response.setHeader("Content-Type", "application/json");
 
     try {
-        const apiResponse = await axios.get("https://www.fruityvice.com/api/fruit/apple");
-
+        const apiResponse = await axios.get(`https://www.fruityvice.com/api/fruit/${fruit}`);
         response.statusCode = 200;
         response.end(JSON.stringify(apiResponse.data));
     } catch (error) {
         response.statusCode = 500;
         response.end(JSON.stringify({ error: "Failed to fetch data", message: error.message}));
     }
-
 });
 
 server.listen(port, hostname, () => {
-    console.log(`Server successfully running at http://${hostname}:${port}/`);
+    console.log(`Server successfully running at http://${hostname}:${port}/${fruit}`);
 });
-
-
