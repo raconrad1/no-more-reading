c5442f203a07bfb41a24deb6e5d4a07c480f388e
ryanabdallahconrad <ryanconrad1@zohomail.com>
2025-01-15 14:21:02 -0600

    added comments

--- database.js
+++ database.js
@@ -2,6 +2,7 @@ import mysql from "mysql2";
 import dotenv from "dotenv";
 dotenv.config();
 
+// createPool is used to manage a pool of mysql connections, using the .env variables in my own .env file to create a connection on my local host without that information displayed in the source code.
 const pool = mysql.createPool( {
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
@@ -9,11 +10,13 @@ const pool = mysql.createPool( {
     database: process.env.MYSQL_DATABASE || "notes_app"
 }).promise();
 
+//Returns an array of note objects
 export async function getNotes() {
     const [rows] = await pool.query("SELECT * FROM notes");
     return rows;
 }
 
+// Returns a single note object given the id requested by the user during the HTTP request
 export async function getNote(id) {
     const [rows] = await pool.query(`
     SELECT * 
@@ -23,6 +26,7 @@ export async function getNote(id) {
     return rows[0];
 }
 
+// Creates a new note given the user's title and contents, then returns the new note object
 export async function createNote(title, contents) {
     const [result] = await pool.query(`
     INSERT INTO notes (title, contents)
