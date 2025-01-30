0d79d43272ef14e659b989c0aaec22650af736a7
ryanabdallahconrad <ryanconrad1@zohomail.com>
2025-01-30 11:28:35 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,101 @@
+import express from "express";
+import {getFruits, getFruitName, getFruitId, getFruitFamily, addFruit} from "./database.js";
+
+// Creates the express app
+const app = express();
+app.use(express.json());
+
+// Fetch all fruits (GET /fruits/all)
+app.get("/fruits/all", async (req, res) => {
+    try {
+        const fruits = await getFruits();
+        res.send(fruits);
+    } catch (error) {
+        console.error("Error fetching all fruits:" , error.message);
+        res.status(500).send({ error: "Internal server error" });
+    }
+})
+
+// Fetch fruit by name
+app.get("/fruits/name/:name", async (req, res) => {
+    const name = req.params.name;
+    try {
+        const fruit = await getFruitName(name);
+        if(!fruit) {
+            return res.status(400).send("Fruit not found!");
+        }
+
+        res.send(fruit);
+    } catch (error) {
+        console.error("Error fetching fruit by name:", error.message);
+        res.status(500).send({ error: "Internal server error" });
+    }
+})
+
+// Fetch fruit by ID
+app.get("/fruits/id/:id", async (req, res) => {
+    const id = parseInt(req.params.id);
+    if(isNaN(id) || id <= 0) {
+        return res.status(404).send("Invalid ID: ID must be a number greater than 0");
+    }
+    try {
+        const fruit = await getFruitId(id);
+
+        if(!fruit) {
+            return res.status(404).send("Fruit not found!");
+        }
+
+        res.send(fruit);
+    } catch (error) {
+        console.error("Error fetching fruit by ID:" ,error.message);
+        res.status(500).send({ error: "Internal server error" });
+    }
+})
+
+// Fetch fruit by family name
+app.get("/fruits/family/:family", async (req, res) => {
+    const family = req.params.family;
+
+    try {
+        const fruit = await getFruitFamily(family);
+        if(!fruit) {
+            return res.status(404).send(`Fruit not found! There are not fruits in the database in the ${family} family.`);
+        }
+        res.send(fruit);
+    } catch (error) {
+        console.error("Error fetching fruit by family:", error.message);
+        res.status(500).send({ error: "Internal server error"});
+    }
+})
+
+// Add fruit
+app.post("/fruits/add", async (req, res) => {
+    const {name, family, order, genus, calories, fat, sugar, carbohydrates, protein} = req.body;
+
+    if (!name || !family || !order || !genus || calories == null || fat == null || sugar == null || carbohydrates == null || protein == null){
+        return res.status(400).send("Missing required fields in request body!");
+    }
+
+    try {
+        const newFruit = await addFruit(name, family, order, genus, calories, fat, sugar, carbohydrates, protein);
+
+        res.status(201).send({
+            message: "Fruit added successfully!",
+            fruit: newFruit,
+        });
+    } catch(error) {
+        console.error("Error adding fruit:", error.message);
+        res.status(500).send({error: "Internal server error"});
+    }
+});
+
+// Error handling
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+})
+
+// Start the server
+app.listen(8080, () => {
+    console.log(`Server is running on port 8080`);
+});
---
+++ database.js
@@ -0,0 +1,70 @@
+import mysql from "mysql2";
+import dotenv from "dotenv";
+dotenv.config();
+
+// createPool is used to manage a pool of mysql connections, using the .env variables in my own .env file to create a connection on my local host without that information displayed in the source code.
+const pool = mysql.createPool( {
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
+}).promise();
+
+//Returns an array of fruit objects
+export async function getFruits() {
+    const [res] = await pool.query("SELECT * FROM fruits");
+    return res;
+}
+
+// Returns a single fruit via name
+export async function getFruitName(name) {
+    const [res] = await pool.query(`
+    SELECT * 
+    FROM fruits
+    WHERE name = ?
+    `, [name]);
+    return res[0];
+}
+// Returns a single fruit via id
+export async function getFruitId(id) {
+    const [res] = await pool.query(`
+    SELECT *
+    FROM fruits
+    WHERE id = ?
+    `, [id]);
+    return res[0];
+}
+
+// Returns an array of fruit objects via family name
+export async function getFruitFamily(family) {
+    const [res] = await pool.query(`
+    SELECT *
+    FROM fruits
+    WHERE family = ?
+    `, [family]);
+    return res
+}
+
+// Adds a fruit to the database
+export async function addFruit(name, family, order, genus, calories, fat, sugar, carbohydrates, protein) {
+    try {
+        const [res] = await pool.query(`
+                    INSERT INTO fruits (name, family, \`order\`, genus, calories, fat, sugar, carbohydrates, protein)
+                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+            [name, family, order, genus, calories, fat, sugar, carbohydrates, protein]);
+        return {
+            name,
+            family,
+            order,
+            genus,
+            calories,
+            fat,
+            sugar,
+            carbohydrates,
+            protein
+        };
+    } catch(error) {
+        console.error("Error adding fruit:", error);
+        throw error;
+    }
+}
